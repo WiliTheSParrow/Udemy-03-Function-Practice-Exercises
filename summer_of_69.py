bf385f51f2aa81e0545c1455b32850c21e3b3203
WiliTheSparrow <w.julia91@gmail.com>
2019-02-25 10:04:42 +0100

    practice

--- summer_of_69.py
+++ summer_of_69.py
@@ -1,15 +1,29 @@
-# SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
+# SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with
+# a 6 and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
 # summer_69([1, 3, 5]) --> 9
 # summer_69([4, 5, 6, 7, 8, 9]) --> 9
 # summer_69([2, 1, 6, 9, 11]) --> 14
 
 def summer_69(arr):
-    for item in arr:
-        if item == 6:
-            
-        else:
-            return sum(arr)
-    
+    numsum = 0
+    adding = True
+
+    for num in arr:
+        while adding:
+            if num != 6:
+                numsum += num
+                break
+            else:
+                adding = False
+        while not adding:
+            if num != 9:
+                break
+            else:
+                adding = True
+                break
+    return numsum
+
+
 # Check
 summer_69([1, 3, 5])
 
